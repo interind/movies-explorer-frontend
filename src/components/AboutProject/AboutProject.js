722f61b5a10302ed0bad4221d636fd00dba8d248
Aleksandr <interind@yandex.ru>
2021-02-23 23:36:57 +0300

    AboutProject 90%

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,35 +1,44 @@
 import React from 'react';
-import './AboutProject.css';
 import HeaderBar from '../HeaderBar/HeaderBar';
+import cryptoKeys from '../../utils/crypto';
+import './AboutProject.css';
 
 function AboutProject() {
   const aboutProject = {
     title: 'О проекте',
     place: 'aboutProject',
   };
+  const dataInfo = [
+    {
+      title: 'Дипломный проект включал 5 этапов',
+      description: 'Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.',
+    },
+    {
+      title: 'На выполнение диплома ушло 5 недель',
+      description: 'У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.',
+    },
+  ];
+  const timeInfo = [
+    {
+      title: '1 неделя',
+      description: 'Back-end',
+    },
+    {
+      title: '4 недели',
+      description: 'Front-end',
+    },
+  ];
   return (
     <section className='AboutProject' id={'about-project'}>
       <HeaderBar modifier={aboutProject} />
-      <div className='AboutProject__containers'>
-        <div className='AboutProject__container'>
-          <h3 className='AboutProject__title AboutProject__title_size_m'>Дипломный проект включал 5 этапов</h3>
-          <p className='AboutProject__title AboutProject__title_theme_s'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
-        </div>
-        <div className='AboutProject__container'>
-          <h3 className='AboutProject__title AboutProject__title_size_m'>На выполнение диплома ушло 5 недель</h3>
-          <p className='AboutProject__title AboutProject__title_theme_s'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-        </div>
-      </div>
-      <div className='AboutProject__containers'>
-        <div className='AboutProject__container AboutProject__container_size_m'>
-          <h3 className='AboutProject__title AboutProject__title_theme_green'>1 неделя</h3>
-          <p className='AboutProject__title AboutProject__title_theme_white'>Back-end</p>
-        </div>
-        <div className='AboutProject__container AboutProject__container_size_x'>
-          <h3 className='AboutProject__title AboutProject__title_theme_gray'>4 недели</h3>
-          <p className='AboutProject__title AboutProject__title_theme_white'>Front-end</p>
-        </div>
-      </div>
+      { dataInfo.map((info) => <div key={cryptoKeys(info.title)} className='AboutProject__container'>
+          <h3 className='AboutProject__title'>{info.title}</h3>
+          <p className='AboutProject__subtitle'>{info.description}</p>
+      </div>)}
+      { timeInfo.map((info) => <div key={cryptoKeys(info.title)} className='AboutProject__container'>
+          <h3 className={`AboutProject__title AboutProject__title_theme_${info.title.includes('1') ? 'green' : 'gray'}`}>{info.title}</h3>
+          <p className='AboutProject__subtitle AboutProject__subtitle_theme_gray'>{info.description}</p>
+      </div>)}
     </section>
   );
 }
