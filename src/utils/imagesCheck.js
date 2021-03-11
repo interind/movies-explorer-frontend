8d388daa92bb895dfa5c36d7c73246b30968eb30
Aleksandr <interind@yandex.ru>
2021-03-11 10:43:40 +0300

    delete utils check images

--- src/utils/imagesCheck.js
+++
@@ -1,8 +0,0 @@
-const imagesCheck = (link) => new Promise((resolve, reject) => {
-  const image = new Image();
-  image.src = `https://api.nomoreparties.co${link}`;
-  image.onload = () => resolve();
-  image.onerror = () => reject(new Error('ошибка ссылки'));
-});
-
-export default imagesCheck;
