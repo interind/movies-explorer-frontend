0d2be2af3a141eac0b502e9767f019d7af8f276c
Aleksandr <interind@yandex.ru>
2021-03-21 13:46:42 +0300

    infoTool close

--- src/components/InfoTool/InfoTool.js
+++ src/components/InfoTool/InfoTool.js
@@ -1,20 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './InfoTool.css';
 
 function InfoTool({ data, toggleEventListenerWindowClick }) {
   const { message = '', type = '', visible } = data;
+  const [status, setStatus] = useState(visible);
   useEffect(() => {
     if (visible) {
       toggleEventListenerWindowClick(true);
+      setStatus(visible);
     }
     return () => {
       toggleEventListenerWindowClick(false);
+      setTimeout(() => {
+        setStatus(!visible);
+      }, 3000);
     };
   }, [visible, toggleEventListenerWindowClick]);
   return (
     <React.Fragment>
-      {visible && <div className='InfoTool'>
+      {status && <div className='InfoTool'>
         {!type ? <h2 className='InfoTool__title InfoTool__title_theme_red'>{message}</h2>
           : <h2 className='InfoTool__title InfoTool__title_theme_green'>{message}</h2>}
       </div>}
